66edd3a39a5e8686296df5b69a096d6894fa9854
Konrad Rej <31326411+konradrej@users.noreply.github.com>
2021-01-22 13:08:50 +0100

    Expanded Cell.

--- src/Cell.java
+++ src/Cell.java
@@ -3,5 +3,44 @@ import javax.swing.*;
 public class Cell extends JButton {
     private int x;
     private int y;
-    private int value;
+    private String value;
+
+    public Cell(){
+
+    }
+
+    public Cell(int x, int y, int value){
+        this.x = x;
+        this.y = y;
+        this.setValue(value);
+    }
+
+    public void setX(int x){
+        this.x = x;
+    }
+
+    public int getX(){
+        return x;
+    }
+
+    public void setY(int y){
+        this.y = y;
+    }
+
+    public int getY(){
+        return y;
+    }
+
+    public void setValue(int value){
+        if(value == 0){
+            this.value = "";
+        }else{
+            this.value = String.valueOf(value);
+        }
+
+    }
+
+    public String getValue(){
+        return value;
+    }
 }

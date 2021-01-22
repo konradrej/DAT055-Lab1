86ca75e071ce5aa48b37a3e8a5aebd06999708d4
Konrad Rej <31326411+konradrej@users.noreply.github.com>
2021-01-22 13:54:42 +0100

    Temporary main code.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,37 @@
-public class Main {
-    public static void main(String[] args){
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
 
+public class Main {
+
+    public static void main(String[] args){
+        JFrame frame = new JFrame("ImageViewer");
+        JPanel panel = new JPanel();
+
+        ArrayList<Cell> cells = new ArrayList<>();
+        GameGenerator generator = new GameGenerator(4);
+
+        int j = 0, k = 0;
+        for(int i : generator){
+            Cell cell = new Cell(j++, k, i);
+            cell.setVisible(true);
+            cells.add(cell);
+
+            if(j == 4){
+                k++;
+                j = 0;
+            }
+        }
+
+        for(Cell cell : cells){
+            panel.add(cell);
+        }
+
+        frame.setPreferredSize(new Dimension(600, 600));
+        panel.setVisible(true);
+        panel.setLayout(new GridLayout(4,4,3,3));
+        frame.add(panel);
+        frame.pack();
+        frame.setVisible(true);
     }
 }
